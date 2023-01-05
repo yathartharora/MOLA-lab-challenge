d1dd554bf41d07144a1b1f5b722570b4f73e9848
Yatharth Arora <yathartharora1999@gmail.com>
2023-01-05 11:19:25 -0800

    Tweet DOM pattern

--- content.js
+++ content.js
@@ -1,9 +1,20 @@
 chrome.runtime.onMessage.addListener(
+    // console.log(tweets)
     function(request, sender, sendResponse) {
       console.log(sender.tab ?
                   "from a content script:" + sender.tab.url :
                   "from the extension");
     console.log(request)
+    const t = document.getElementsByClassName("css-1dbjc4n r-1iusvr4 r-16y2uox r-1777fci r-kzbkwu")
+    // const t = tweets.getElementsByClassName("css-1dbjc4n")
+    for(i=0;i<t.length;i++){
+        // console.log(t[i])
+        const res = t[i].getElementsByClassName("css-901oao r-1nao33i r-37j5jr r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-bnwqim r-qvutc0")
+        const r = res[0].getElementsByClassName("css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0")
+        for(j=0;j<r.length;j++){
+            console.log(r[j].innerHTML)
+        }
+    }
       if (request.greeting === "hello")
         sendResponse({farewell: "goodbye"});
     }
--- extension.js
+++ extension.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', function() {
     var button = document.getElementById('permission');
     button.addEventListener('click', async function () {
         await chrome.tabs.query({active: true, currentWindow: true}, async function(tabs) {
-
             const response = await chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"});
             console.log(response)
         });
