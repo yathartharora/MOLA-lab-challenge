dc735c3e54ffb2c8112f353af6dd266ea9a246d3
Yatharth Arora <yathartharora1999@gmail.com>
2023-01-05 13:45:08 -0800

    Mood Detection Added

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
+    async function(request, sender, sendResponse) {
       console.log(sender.tab ?
                   "from a content script:" + sender.tab.url :
                   "from the extension");
@@ -7,27 +7,20 @@ chrome.runtime.onMessage.addListener(
     // const t = tweets.getElementsByClassName("css-1dbjc4n")
     tweets = []
     for(i=0;i<t.length;i++){
-        // console.log(t[i])
         const res = t[i].getElementsByClassName("css-901oao r-1nao33i r-37j5jr r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-bnwqim r-qvutc0")
         const r = res[0].getElementsByClassName("css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0")
 
         const h = t[i].getElementsByClassName("css-1dbjc4n r-1awozwy r-18u37iz r-1wbh5a2 r-dnmrzs r-1ny4l3l")
         const m = h[0].getElementsByClassName("css-1dbjc4n r-18u37iz r-1wbh5a2 r-13hce6t")
-        // console.log(m[0])
         const l = m[0].getElementsByClassName("css-1dbjc4n r-18u37iz r-1q142lx")
-        console.log(l[0])
-        const addEmoji = "<div class=\"css-1dbjc4n r-18u37iz r-1q142lx\"><span class=\"css-4rbku5 css-18t94o4 css-901oao r-1bwzh9t r-1loqt21 r-xoduu5 r-1q142lx r-1w6e6rj r-37j5jr r-a023e6 r-16dba41 r-9aw3ui r-rjixqe r-bcqeeo r-3s2u2q r-qvutc0\">Hello World</span> </div>"
-        m[0].innerHTML = m[0].innerHTML + addEmoji
-        console.log('####')
         temp = []
         for(j=0;j<r.length;j++){
-            console.log(r[j].innerHTML)
             temp.push(r[j].innerHTML)
         }
         tweets.push({"tweet_text": temp.join(" ")})
     }
 
-    fetch('http://127.0.0.1:5000/api/language-detection', {
+    await fetch('http://127.0.0.1:5000/api/language-detection', {
     method: 'POST',
     headers: {
         'Accept': 'application/json',
@@ -45,10 +38,40 @@ chrome.runtime.onMessage.addListener(
             },
             body: JSON.stringify(response)
         }).then(response => response.json())
-        .then(response => console.log(response))
+        .then((response) => {
+            console.log(response)
+            updateDOM(t,response)
+        })
       }
       )
     if (request.greeting === "hello")
         sendResponse({farewell: "goodbye"});
     }
   );
+
+
+  function updateDOM(t,res){
+    // console.log(t)
+    for(k=0;k<t.length;k++){
+        console.log(res[k]["detected_mood"])
+        const h = t[k].getElementsByClassName("css-1dbjc4n r-1awozwy r-18u37iz r-1wbh5a2 r-dnmrzs r-1ny4l3l")
+        const m = h[0].getElementsByClassName("css-1dbjc4n r-18u37iz r-1wbh5a2 r-13hce6t")
+        const l = m[0].getElementsByClassName("css-1dbjc4n r-18u37iz r-1q142lx")
+        // console.log(l[0])
+        if(res[k]["detected_mood"]=="POSITIVE"){
+            const addEmoji = "<div class=\"css-1dbjc4n r-18u37iz r-1q142lx\"><span class=\"css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0\">·</span><span class=\"css-4rbku5 css-18t94o4 css-901oao r-1bwzh9t r-1loqt21 r-xoduu5 r-1q142lx r-1w6e6rj r-37j5jr r-a023e6 r-16dba41 r-9aw3ui r-rjixqe r-bcqeeo r-3s2u2q r-qvutc0\"> Detected Mood: 😊</span> </div>"
+            m[0].innerHTML = m[0].innerHTML + addEmoji
+        }
+        else if(res[k]["detected_mood"]=="NEUTRAL"){
+            const addEmoji = "<div class=\"css-1dbjc4n r-18u37iz r-1q142lx\"><span class=\"css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0\">·</span><span class=\"css-4rbku5 css-18t94o4 css-901oao r-1bwzh9t r-1loqt21 r-xoduu5 r-1q142lx r-1w6e6rj r-37j5jr r-a023e6 r-16dba41 r-9aw3ui r-rjixqe r-bcqeeo r-3s2u2q r-qvutc0\"> Detected Mood: 😐</span> </div>"
+            m[0].innerHTML = m[0].innerHTML + addEmoji
+        }else if(res[k]["detected_mood"]=="NEGATIVE"){
+            const addEmoji = "<div class=\"css-1dbjc4n r-18u37iz r-1q142lx\"><span class=\"css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0\">·</span><span class=\"css-4rbku5 css-18t94o4 css-901oao r-1bwzh9t r-1loqt21 r-xoduu5 r-1q142lx r-1w6e6rj r-37j5jr r-a023e6 r-16dba41 r-9aw3ui r-rjixqe r-bcqeeo r-3s2u2q r-qvutc0\"> Detected Mood: 🙁</span> </div>"
+            m[0].innerHTML = m[0].innerHTML + addEmoji
+        } else{
+            const addEmoji = "<div class=\"css-1dbjc4n r-18u37iz r-1q142lx\"><span class=\"css-4rbku5 css-18t94o4 css-901oao r-1bwzh9t r-1loqt21 r-xoduu5 r-1q142lx r-1w6e6rj r-37j5jr r-a023e6 r-16dba41 r-9aw3ui r-rjixqe r-bcqeeo r-3s2u2q r-qvutc0\"></span> </div>" 
+            m[0].innerHTML = m[0].innerHTML + addEmoji
+        }
+        
+    }
+  }
--- extension.js
+++ extension.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // document.getElementById('status').textContent = "Extension loaded";
+    document.getElementById('status').textContent = "Extension loaded";
+    document.getElementById('content').textContent = "Click on Load Extension to detect mood of the tweets.";
     var button = document.getElementById('permission');
     button.addEventListener('click', async function () {
         await chrome.tabs.query({active: true, currentWindow: true}, async function(tabs) {
